55252e029bb02b6f182859fb38671ff170749886
IAMPAVANJ <pavanjadhav45710@gmail.com>
2025-06-08 22:54:02 +0530

    origin update in backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,23 +4,7 @@ const express = require('express');
 const app = express();
 const AiRoutes = require('./routes/ai.routes');
 const cors = require('cors');
-const allowedOrigins = [
-  "https://reviewer-ten.vercel.app",
-  "http://localhost:5173"
-];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    // allow requests with no origin (like mobile apps or curl)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    } else {
-      return callback(new Error("Not allowed by CORS"));
-    }
-  },
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-}));
+app.use(cors());
 app.use(express.json());
 app.use('/ai', AiRoutes);
 
